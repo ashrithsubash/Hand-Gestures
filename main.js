13f457784fc0d3fa06597b5263557e0cbcfef81b
ashrithsubash <121907505+ashrithsubash@users.noreply.github.com>
2023-07-20 17:30:50 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,75 @@
-//https://teachablemachine.withgoogle.com/models/4U_7WEHnL/
+prediction_1 = ""
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+      
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/4U_7WEHnL/model.json',modelLoaded);
+
+function speak(){
+  var synth = window.speechSynthesis;
+  speak_data_1 = "The prediction is " + prediction_1;
+  var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+  synth.speak(utterThis);
+}
+
+function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML = results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak();
+    if(results[0].label == "good")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128077;";
+    }
+    if(results[0].label == "perfect")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128076;";
+    }
+    if(results[0].label == "bad")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128078;";
+    }
+    if(results[0].label == "bye")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128075;";
+    }
+    if(results[0].label == "clap")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128079;";
+    }
+    if(results[0].label == "hands raised in victory")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#9994;";
+    }
+  }
+}
